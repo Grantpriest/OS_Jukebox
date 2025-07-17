ea7e10472159ba2146b1a4cee8065ab5dfba00ca
Grantpriest <grantpriest@gmail.com>
2025-07-18 11:27:43 +1200

    5

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/OS_Jukebox/', // 👈 must match your GitHub repo name
+  base: '/OS_Jukebox/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
 })
